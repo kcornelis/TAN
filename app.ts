fd39b51281e1c45526bb185bd375c93c4255131d
kcornelis <kevin.cornelis@outlook.com>
2016-02-28 15:09:45 +0100

    Use ts-node + express demo

--- app.ts
+++ app.ts
@@ -1,8 +1,24 @@
-class App {
-    sayHello() {
-        console.log('hello from ts');
+import * as express from 'express';
+
+export class App {
+    
+    app: express.Express;
+    
+    constructor(){
+        this.app = express();
+        this.initializeExpress();
+    }
+    
+    start() {
+
+        this.app.listen(3000, function () {
+            console.log('Example app listening on port 3000!');
+        });
+    }
+    
+    private initializeExpress(){
+        this.app.get('/', function (req, res) {
+            res.send('Hello World!');
+        });
     }
 }
-
-var app =  new App();
-app.sayHello();
--- server.js
+++
@@ -1,4 +0,0 @@
-require('typescript-require');
-require('./app.ts');
-
-console.log('server.js finished');
